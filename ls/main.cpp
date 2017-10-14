d34a4e3fdb682f62e2e713fd3e79c2723fc341cb
NoNameA 774 <nonamea774@gmail.com>
2017-10-14 23:32:02 +0900

    impl simple ls

---
+++ ls/main.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using std::begin;
+using std::end;
+
+int main(int argc, char* argv[]) {
+  std::string const target = (argc < 2) ? "." : argv[1];
+  DIR *dirp = opendir(target.c_str());
+  struct dirent* ent;
+  std::vector<std::string> names;
+
+  while (ent = readdir(dirp), ent != NULL) {
+    names.push_back(ent->d_name);
+  }
+  std::sort(begin(names), end(names));
+
+  for(auto const &e: names) {
+    std::cout << e << std::endl;
+  }
+  return 0;
+}
+
+
+
+
+
+
+
